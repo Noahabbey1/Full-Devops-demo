fa3f03f238a544d054218147f0493598fc4cf6f1
Noah <noahabbey23@gmail.com>
2026-05-28 13:05:04 -0300

    microservices setup

---
+++ order-service/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/orders')
+def orders():
+    return jsonify({
+        "message": [
+            "order-101",
+            "order-202",
+            "order-303"
+        ]
+    })
+
+if __name__ == '__main__':
+    app.run(port=5003, debug=True)
